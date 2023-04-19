d2f0e778e9077add2852ab207e7969102eda2722
ssrguest <96939386+ssrguest@users.noreply.github.com>
2023-04-19 13:10:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 # 默认各参数值，请自行修改.(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 PORT=${PORT:-'8080'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-WSPATH=${WSPATH:-'argo'}
+UUID=${UUID:-'161bbb1b-6b82-427a-80ca-fb7a92f8f6c4'}
+WSPATH=${WSPATH:-'f6c4'}
 
 # 生成 Xray 配置文件
 cat > config.json << EOF
